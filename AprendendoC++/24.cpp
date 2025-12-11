6ed58367197ad7f51394c4c3a486f618c06b5be6
antonio-djls <antoniodinizjorgelimasaraiva@gmail.com>
2025-12-11 13:35:47 -0300

    11-12-25

--- AprendendoC++/24.cpp
+++ AprendendoC++/24.cpp
@@ -8,8 +8,11 @@ class UDT{
 };
 
 int main(){
+    
+    // Criando um único ponteiro
     std::unique_ptr<UDT> antonio = std::unique_ptr<UDT>(new UDT);
-    
-    
+    // Criando um vetor que é referenciado por um unico ponteiro
+    // std::unique_ptr<UDT[]> antonio_vetor = std::make_unique<UDT[]>(100);
+    std::unique_ptr<UDT> outro = std::move(antonio);
     return 0;
 }
