4a7ad5ad6c7b304e9b20b1e727500ed146292306
antonio-djls <antoniodinizjorgelimasaraiva@gmail.com>
2025-12-13 00:10:46 -0300

    13-12-25

--- Competitive Programming/1.cpp
+++ Competitive Programming/1.cpp
@@ -1,23 +1,24 @@
-#include<iostream>
-#include<vector>
-
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main(){
-    std::vector<int> vetor = {1,3,8,2,9,2,5,6};
+int main() {
+  std::vector<int> vetor = {1, 3, 8, 2, 9, 2, 5, 6};
 
-    for(int k = 0; k < vetor.size(); k++){
-        for(int j = k + 1; j < vetor.size(); j++){
-            if(vetor[k] > vetor[j] ){
-                swap(vetor[k],vetor[j]);
-            }   
-        }
+  for (int k = 0; k < vetor.size(); k++) {
+    for (int j = k + 1; j < vetor.size(); j++) {
+      if (vetor[k] > vetor[j]) {
+        swap(vetor[k], vetor[j]);
+      }
     }
-    for(auto inteiro : vetor){
-        cout << inteiro << endl;
-    }
+  }
+  for (auto inteiro : vetor) {
+    cout << inteiro << endl;
+  }
+  for (int k = 0; k < 10; k++) {
+    cout << "Antonio Diniz Jorge lima Saraiva \n";
+  }
 
-    
-    return 0;
+  return 0;
 }
