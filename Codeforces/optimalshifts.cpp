ffdb268551969b9ab6a35535167f074ee0425baf
antonio-djls <antoniodinizjorgelimasaraiva@gmail.com>
2026-01-11 15:10:47 -0300

    11.01.26

---
+++ Codeforces/optimalshifts.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x{}; cin >> x;
+    string input;
+    int size{};
+    while(x--){
+        cin >> size;
+        cin >> input;
+
+        int zeros =  count(input.begin(),input.end(),0);
+        int ums =  count(input.begin(),input.end(),1);
+        if(zeros > ums){
+            cout << zeros-ums << endl;
+        }else if(ums > zeros){
+            cout << ums - zeros << endl;
+        }else{
+            cout << 0  << endl;
+        }
+
+    }
+
+
+    return 0;
+}
