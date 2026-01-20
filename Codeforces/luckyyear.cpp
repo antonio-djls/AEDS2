45355752b479d739308e2f6cec578bc544abdd85
antonio-djls <antoniodinizjorgelimasaraiva@gmail.com>
2026-01-19 22:47:59 -0300

    19.01.26

--- Codeforces/luckyyear.cpp
+++ Codeforces/luckyyear.cpp
@@ -7,6 +7,22 @@ int main(){
     ios::sync_with_stdio(false);    cin.tie(nullptr);
 
     ll int x{}; cin >> x;
+    ll int y  = x;
+    if(x < 10){
+        cout << 1 << endl; return 0;
+    }else{
+        int count = 0; int ultimo = 0;
+        // vector<int> vi;s
+        while(x > 0){
+            // cout << x % 10<< endl;
+            ultimo = x % 10;
+            x/=10;
+            count++;
+        }
+        ultimo++;
+        int ans = pow(10,count-1);
+        cout << ans * ultimo-y << endl;
+    }
 
 
     return 0;
